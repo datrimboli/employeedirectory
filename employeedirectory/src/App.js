1d670ab174f606614ee9fe204578ce9d003afb8d
Dave Trimboli <davidatrimboli@gmail.com>
2020-09-16 18:09:51 -0400

    react created, gutted, imports, axios, v6

---
+++ employeedirectory/src/App.js
@@ -0,0 +1,20 @@
+import React, {useState, useEffect } from 'react'
+import 'react-table-v6/react-table.css'
+import axios from 'axios'
+import ReactTable from 'react-table-v6'
+
+const App = () => {
+  
+  
+
+return (
+
+
+<h1>Hello</h1>
+
+)
+
+
+}
+
+export default App
