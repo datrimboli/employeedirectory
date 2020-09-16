ef6533108bf99a73205f64afa3b787a0962fb7ba
Dave Trimboli <davidatrimboli@gmail.com>
2020-09-16 18:48:45 -0400

    link to axios, name, phone, email, rendering

--- employeedirectory/src/App.js
+++ employeedirectory/src/App.js
@@ -1,20 +1,53 @@
-import React, {useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import 'react-table-v6/react-table.css'
 import axios from 'axios'
 import ReactTable from 'react-table-v6'
 
 const App = () => {
-  
-  
 
-return (
+  const [employeeState, setEmployeeState] = useState({
+    employees: [],
+    columns: [
+      {
+        Header: 'Name',
+        accessor: 'name'
+      },
+      {
+        Header: 'Phone',
+        accessor: 'phone'
+      },
+      {
+        Header: 'Email',
+        accessor: 'email'
+      },
+    ]
+  })
+
+  useEffect(() => {
+    axios.get('https://randomuser.me/api?results=20')
+      .then(({ data }) => {
+        console.log(data.results)
+
+        let employees = data.results.map(employee => ({
+          name: employee.name.first + ' ' + employee.name.last,
+          email: employee.email,
+          phone: employee.phone
+        }))
+        setEmployeeState({ ...employeeState, employees })
+      })
+      .catch(err => console.error(err))
+  }, [])
+
+  return (
+    <ReactTable
+      data={employeeState.employees}
+      columns={employeeState.columns}
+    />
 
 
-<h1>Hello</h1>
 
-)
-
-
+  )
 }
 
 export default App
+
