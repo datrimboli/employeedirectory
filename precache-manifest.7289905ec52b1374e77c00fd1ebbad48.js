2c3b4181ba05bbff32a1dfd27f1bd0c5e50aa730
Dave Trimboli <davidatrimboli@gmail.com>
2020-09-17 19:47:53 -0400

    Updates

---
+++ precache-manifest.7289905ec52b1374e77c00fd1ebbad48.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35eab49e8aeff34c793ec176cdad2ebb",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "19f5465b07a75103d114",
+    "url": "/react-gh-pages/static/css/2.bfa46653.chunk.css"
+  },
+  {
+    "revision": "05bc5eb94e145e5e9c08",
+    "url": "/react-gh-pages/static/css/main.1107e288.chunk.css"
+  },
+  {
+    "revision": "19f5465b07a75103d114",
+    "url": "/react-gh-pages/static/js/2.64f63020.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-gh-pages/static/js/2.64f63020.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05bc5eb94e145e5e9c08",
+    "url": "/react-gh-pages/static/js/main.5e59db8b.chunk.js"
+  },
+  {
+    "revision": "99ac08b993a69d73fc78",
+    "url": "/react-gh-pages/static/js/runtime-main.3e4cfadf.js"
+  }
+]);
